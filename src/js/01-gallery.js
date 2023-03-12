c2c07f64b19156a5b7a17eeb12554f1122181cfe
Elena Us <Elena88555@gmail.com>
2023-03-12 16:02:28 +0200

    goit-js-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+
+
+
+const markup = createCards(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+function createCards(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__item"
+href="${original}">
+<img class="gallery__image"
+src="${preview}"
+alt="Image ${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom"
+  });
+
+
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const inputEmail = document.querySelector('input[name="email"]')
+const messageText = document.querySelector('textarea[name = "message"]')
+
+
+
+form.addEventListener("submit", handleSubmitForm)
+
+
+function handleSubmitForm (event) {
+event.preventDefault()
+event.currentTarget.reset()
+localStorage.removeItem("feedback-form-state")
+
+if (inputEmail.value === "" || messageText.value === "" ) {
+return alert("Всі поля мають бути заповнені")
+}
+}
+
+textareaMessage()
+let formData = {}
+
+form.addEventListener("input", throttle(handleInputTextarea, 500))
+
+function handleInputTextarea (event) {
+  formData[event.target.name] = event.target.value
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+console.log( formData[event.target.value])
+}
+
+
+function textareaMessage () {
+  const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"))
+  if (savedMessage) {
+inputEmail.value = savedMessage.email 
+messageText.value = savedMessage.message
+console.log(inputEmail.value);
+console.log(messageText.value);
+  }
+}
+
 
