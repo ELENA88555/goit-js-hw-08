ad2db9d394b793f576822c9b631b25fd80a4e36a
Elena Us <Elena88555@gmail.com>
2023-03-13 18:45:57 +0200

    goit-js-hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,41 +4,43 @@ const form = document.querySelector(".feedback-form");
 const inputEmail = document.querySelector('input[name="email"]')
 const messageText = document.querySelector('textarea[name = "message"]')
 
-
-
-form.addEventListener("submit", handleSubmitForm)
-
-
-function handleSubmitForm (event) {
-event.preventDefault()
-event.currentTarget.reset()
-localStorage.removeItem("feedback-form-state")
-
-if (inputEmail.value === "" || messageText.value === "" ) {
-return alert("Всі поля мають бути заповнені")
-}
-}
-
 textareaMessage()
 let formData = {}
+
 
 form.addEventListener("input", throttle(handleInputTextarea, 500))
 
 function handleInputTextarea (event) {
   formData[event.target.name] = event.target.value
   localStorage.setItem("feedback-form-state", JSON.stringify(formData))
-console.log( formData[event.target.value])
-}
 
+}
 
 function textareaMessage () {
   const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"))
   if (savedMessage) {
 inputEmail.value = savedMessage.email 
 messageText.value = savedMessage.message
-console.log(inputEmail.value);
-console.log(messageText.value);
+
   }
+
+
+console.log("email", inputEmail.value);
+ console.log("message", messageText.value);
 }
+
+
+form.addEventListener("submit", handleSubmitForm)
+
+function handleSubmitForm (event) {
+event.preventDefault()
+
+if (inputEmail.value === "" || messageText.value === "" ) {
+  return alert("Всі поля мають бути заповнені")
+  } localStorage.removeItem("feedback-form-state")
+  event.currentTarget.reset()
+}
+
+
 
 
